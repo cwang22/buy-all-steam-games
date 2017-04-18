43c5dabdf3fb9d8783f632426dc70e21b962ab40
cwang22 <wcgdream@gmail.com>
2017-04-18 23:43:47 +1000

    change to weekly

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-         $schedule->command('command:fetch')->daily();
+         $schedule->command('command:fetch')->weekly();
     }
 
     /**
