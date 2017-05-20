2c330b42868828c9de7d950ef2cd0fc1d123fb5d
cwang22 <wcgdream@gmail.com>
2017-05-20 11:06:57 +1000

    update schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\Fetch;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,7 +14,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        Commands\Fetch::class
+        Fetch::class
     ];
 
     /**
@@ -24,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-         $schedule->command('fetch')->weekly();
+         $schedule->command(Fetch::class)->weekly()->sendOutputTo('schedule.log');
     }
 
     /**
