a61642081f6261e00e40631da0c4e4b81ac76e12
cwang22 <wcgdream@gmail.com>
2017-08-22 23:22:53 +1000

    add d3 chart

---
+++ resources/assets/js/components/Chart.vue
@@ -0,0 +1,106 @@
+<template>
+    <svg></svg>
+</template>
+<script>
+    import * as d3 from 'd3'
+
+    export default {
+        props: ['records'],
+        mounted() {
+
+            const svg = d3.select("svg")
+            const margin = {
+                top: 20,
+                right: 20,
+                bottom: 60,
+                left: 60
+            }
+            const width = parseInt(svg.style("width")) - margin.left - margin.right
+            const height = width * 0.5625 - margin.top - margin.bottom
+
+            svg.style("height", height + margin.top + margin.bottom);
+            const g = svg.append("g").attr("transform", `translate( ${margin.left}, ${margin.top}`)
+            
+            const parse = d3.timeParse("%Y-%m-%d %H:%M:%S")
+            const x = d3.scaleTime()
+                .rangeRound([0, width])
+            const y = d3.scaleLinear()
+                .rangeRound([height, 0])
+            const line1 = d3.line()
+                .x(function(d) {
+                    return x(d.date);
+                })
+                .y(function(d) {
+                    return y(d.sale);
+                })
+
+            const line2 = d3.line()
+                .x(function(d) {
+                    return x(d.date);
+                })
+                .y(function(d) {
+                    return y(d.original);
+                })
+
+            const data = this.records.map((item) => {
+                return {
+                    date: parse(item.created_at),
+                    sale: item.sale.replace(",", ""),
+                    original: item.original.replace(",", "")
+                }
+            })
+
+
+            x.domain(d3.extent(data, function(d) { return d.date; }));
+            y.domain([d3.min(data, function(d) {
+                return Math.min(d.sale, d.original);
+            }) * 0.9, d3.max(data, function(d) {
+                return Math.max(d.sale, d.original);
+            }) * 1.1]);
+
+            const xAxis = d3.axisBottom(x).ticks(d3.timeWeek)
+            const yAxis = d3.axisLeft(y)
+
+
+            g.append("g")
+                .attr("class", "x axis")
+                .attr("transform", `translate(0, ${height})`)
+                .call(xAxis)
+
+            g.append("g")
+                .attr("class", "y axis")
+                .call(yAxis)
+                .append("text")
+                .attr("y", 6)
+                .attr("x", 80)
+                .attr("dy", "0.71em")
+                .attr("fill", "#000")
+                .text("Total Price ($)");
+
+            g.append("path")
+                .attr("class", "line-1")
+                .attr("fill", "none")
+                .attr("stroke", "steelblue")
+                .attr("stroke-linejoin", "round")
+                .attr("stroke-linecap", "round")
+                .attr("stroke-width", 1.5)
+                .attr("d", line1(data))
+
+            g.append("path")
+                .attr("class", "line-2")
+                .attr("fill", "none")
+                .attr("stroke", "tomato")
+                .attr("stroke-linejoin", "round")
+                .attr("stroke-linecap", "round")
+                .attr("stroke-width", 1.5)
+                .attr("d", line2(data))
+
+        }
+
+    }
+</script>
+<style scoped>
+    svg {
+        width: 100%;
+    }
+</style>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,14 +2,17 @@
 
 @section('content')
     <div class="row">
-        <div id="showcase" class="col-lg-8">
+        <div id="showcase" class="col-lg-12">
             <h1 class="page-header">Buy All Steam Games</h1>
             <p>Ever wonder how much does it costs to buy all games from Steam?</p>
-            <p>Well, it costs <span class="text-danger">${{ $price->sale }}</span> at the moment, which originally
-                costs <span class="text-danger">${{ $price->original }}</span> on full price.</p>
-            <p>This page was last updated {{ $price->updated_at->diffForHumans() }}, based on price of
-                the {{$price->cc}} region and {{$price->language}} language.
+            <p>Well, it costs <span class="text-danger">${{ $record->sale }}</span> at the moment, which originally
+                costs <span class="text-danger">${{ $record->original }}</span> on full price.</p>
+            <p>This page was last updated {{ $record->updated_at->diffForHumans() }}, based on price of
+                the {{$record->cc}} region and {{$record->language}} language.
             </p>
+
+            <h2>Trends</h2>
+            <chart :records="{{ $records }}"></chart>
 
 
             <h2 class="page-header">How does it work?</h2>
@@ -18,7 +21,7 @@
                 exactly the same thing, but haven't updated since 2014.</p>
             <p>This page however is based on PHP and Laravel, and automatically fetch data every week.</p>
             <h2 class="page-header">API</h2>
-            <p>You can hit <code>{{url('api')}}</code> to get the latest data. It should looks like this.</p>
+            <p>You can hit <code>{{url('api')}}</code> to get the all data. It should looks like this.</p>
             <pre>
             {
                 "original": 233448.27,
@@ -37,9 +40,6 @@
                 <!-- Place this tag where you want the button to render. -->
                 <a class="github-button" href="https://github.com/cwang22/buy-all-steam-games" data-icon="octicon-star" data-size="large" aria-label="Star cwang22/buy-all-steam-games on GitHub">Star</a>
             </div>
-        </div>
-        <div class="col-lg-4">
-            <img class="img-responsive" src="{{ asset('images/gaben.jpg') }}" alt="gaben">
         </div>
     </div>
 @endsection
