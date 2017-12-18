093174ef08b9f6d86ce316655488cf6b5f911197
Chenggang Wang <chenggang.wang@designlogic.com.au>
2017-12-18 16:47:42 +1100

    upgrade to 5.5

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,8 +26,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if ($this->app->environment() !== 'production') {
-            $this->app->register(IdeHelperServiceProvider::class);
-        }
+
     }
 }
