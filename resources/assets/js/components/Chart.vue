aa59c5c3f352c8ff8fbbd6045601c45fe64f4915
Chenggang Wang <chenggang.wang@designlogic.com.au>
2018-01-17 13:37:22 +1100

    make yAxes starts at 0

--- resources/assets/js/components/Chart.vue
+++ resources/assets/js/components/Chart.vue
@@ -1,67 +1,75 @@
 <template>
     <div class="wrapper">
-        <canvas id="chart"></canvas> 
+        <canvas id="chart"></canvas>
     </div>
 </template>
 <script>
-import moment from 'moment'
-import Chart from 'chart.js'
-export default {
-  props: ['records'],
-  mounted() {
-    const sale = this.records.map(item => {
-      return {
-        x: new moment(item.created_at),
-        y: new Number(item.sale.replace(',', ''))
-      }
-    })
+    import moment from 'moment'
+    import Chart from 'chart.js'
 
-    const original = this.records.map(item => {
-      return {
-        x: new moment(item.created_at),
-        y: new Number(item.original.replace(',', ''))
-      }
-    })
+    export default {
+        props: ['records'],
+        mounted() {
+            const sale = this.records.map(item => {
+                    return {
+                        x: new moment(item.created_at),
+                        y: new Number(item.sale.replace(',', ''))
+                    }
+                }
+            )
 
-    const ctx = document.getElementById('chart')
+            const original = this.records.map(item => {
+                    return {
+                        x: new moment(item.created_at),
+                        y: new Number(item.original.replace(',', ''))
+                    }
+                }
+            )
 
-    new Chart(ctx, {
-      type: 'line',
-      data: {
-        datasets: [
-          {
-            label: 'Sale Price',
-            data: sale,
-            borderColor: '#0074D9',
-            backgroundColor: 'rgba(0, 116, 217, 0.2)'
-          },
-          {
-            label: 'Orignal Price',
-            data: original
-          }
-        ]
-      },
-      options: {
-        responsive: true,
-        maintainAspectRatio: false,
-        title: {
-          display: true,
-          text: 'Total Price (USD)'
-        },
-        scales: {
-          xAxes: [
-            {
-              type: 'time'
-            }
-          ]
+            const ctx = document.getElementById('chart')
+
+            new Chart(ctx, {
+                type: 'line',
+                data: {
+                    datasets: [
+                        {
+                            label: 'Sale Price',
+                            data: sale,
+                            borderColor: '#0074D9',
+                            backgroundColor: 'rgba(0, 116, 217, 0.2)'
+                        },
+                        {
+                            label: 'Orignal Price',
+                            data: original
+                        }
+                    ]
+                },
+                options: {
+                    responsive: true,
+                    maintainAspectRatio: false,
+                    title: {
+                        display: true,
+                        text: 'Total Price (USD)'
+                    },
+                    scales: {
+                        xAxes: [
+                            {
+                                type: 'time'
+                            }
+                        ],
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    }
+                }
+            })
         }
-      }
-    })
-  }
-}
+    }
 </script>
 <style scoped>
-.wrapper {
-    height: 50vh;
-}
+    .wrapper {
+        height: 50vh;
+    }
 </style>
