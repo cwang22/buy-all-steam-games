20f8f30707a2eaa87d92f699d05dd5a58444be02
Chenggang Wang <chenggang.wang@designlogic.com.au>
2018-03-26 12:19:51 +1100

    design updates

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,24 +3,26 @@
 @section('content')
     <div class="row">
         <div id="showcase" class="col">
-            <h1 class="mt-3 mb-5">Buy All Steam Games <a class="github-button"
-                                                         href="https://github.com/cwang22/buy-all-steam-games"
-                                                         data-icon="octicon-star" data-size="large"
-                                                         aria-label="Star cwang22/buy-all-steam-games on GitHub">Star</a>
-            </h1>
-            <p>Ever wonder how much does it cost to buy all games from Steam?</p>
-            <p>Well, at the moment it costs <span class="text-danger">${{ $record->sale }}</span> at a discounted price which costs<span
-                        class="text-danger">${{ $record->original }}</span> at full price.</p>
-            <p>This page was last updated {{ $record->created_at->diffForHumans() }}, based on the price of
-                the {{$record->cc}} region and {{$record->language}} language.
-            </p>
+            <h1 class="mt-5">Buy All Steam Games</h1>
+            <div class="mt-3">
+                <a class="github-button" href="https://github.com/cwang22" data-size="large" data-show-count="true" aria-label="Follow @cwang22 on GitHub">Follow @cwang22</a>
+                <a class="github-button" href="https://github.com/cwang22/buy-all-steam-games" data-size="large" data-show-count="true" aria-label="Star cwang22/buy-all-steam-games on GitHub">Star</a>
+            </div>
+            <div class="mt-5">
+                <p>Ever wonder how much does it cost to buy all games from Steam?</p>
+                <p>Well, at the moment it costs <span class="text-danger">${{ $record->sale }}</span> at a discounted price which costs<span
+                            class="text-danger">${{ $record->original }}</span> at full price.</p>
+                <p>This page was last updated {{ $record->created_at->diffForHumans() }}, based on the price of
+                    the {{$record->cc}} region and {{$record->language}} language.
+                </p>
+            </div>
 
             <h2 class="mt-5">Trends</h2>
             <chart :records="{{ $records }}"></chart>
 
             <h2 class="mt-5">How does it work?</h2>
             <p>This page was inspired by <a
-                        href="http://buyallofsteam.appspot.com/">http://buyallofsteam.appspot.com/</a>, but it hasn't
+                        href="http://buyallofsteam.appspot.com/" target="_blank">http://buyallofsteam.appspot.com/</a>, but it hasn't
                 been updated since 2014.</p>
             <p>This page however is based on PHP and Laravel, and automatically fetch data every week.</p>
             <h2 class="mt-5">API</h2>
@@ -37,17 +39,6 @@
                 ...
             ]
             </pre>
-
-            <h2 class="mt-5">Github</h2>
-            <p>You can find the Github Repo below, if you like this page, make sure give it a star!</p>
-            <div>
-                <!-- Place this tag where you want the button to render. -->
-                <a class="github-button" href="https://github.com/cwang22" data-size="large"
-                   aria-label="Follow @cwang22 on GitHub">Follow @cwang22</a>
-                <!-- Place this tag where you want the button to render. -->
-                <a class="github-button" href="https://github.com/cwang22/buy-all-steam-games" data-icon="octicon-star"
-                   data-size="large" aria-label="Star cwang22/buy-all-steam-games on GitHub">Star</a>
-            </div>
         </div>
     </div>
 @endsection
