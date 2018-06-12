c540dc1762f08c6f112ed4e38ab89336fae2d211
Chenggang Wang <wcgdream@gmail.com>
2018-06-12 07:14:11 +0000

    Apply fixes from StyleCI

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -34,7 +34,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-	        \JacobBennett\Http2ServerPush\Middleware\AddHttp2ServerPush::class,
+            \JacobBennett\Http2ServerPush\Middleware\AddHttp2ServerPush::class,
         ],
 
         'api' => [
