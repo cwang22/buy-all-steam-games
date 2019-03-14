55d5c1c1e8ff6712f993fd99181f5b994d211af0
Chenggang Wang <chenggang.wang@designlogic.com.au>
2019-03-14 14:46:33 +1100

    update tests

--- tests/Feature/HomepageTest.php
+++ tests/Feature/HomepageTest.php
@@ -2,12 +2,12 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class HomepageTest extends TestCase
 {
-    use DatabaseMigrations;
+    use RefreshDatabase;
 
     /** @test */
     public function it_works()
--- tests/Unit/RecordTest.php
+++ tests/Unit/RecordTest.php
@@ -3,19 +3,19 @@
 namespace Tests\Unit;
 
 use App\Record;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class RecordTest extends TestCase
 {
-    use DatabaseMigrations;
+    use RefreshDatabase;
 
     /** @test */
     public function it_format_prices()
     {
         $record = factory(Record::class)->create([
             'original' => 111111.11,
-            'sale'     => 222222,
+            'sale' => 222222,
         ]);
 
         $this->assertEquals('111,111.11', $record->original);
