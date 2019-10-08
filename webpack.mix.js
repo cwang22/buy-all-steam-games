93451854e2d7b321ce3ac1029113122791685d64
Chenggang Wang <wcgdream@gmail.com>
2019-10-08 23:35:19 +1100

    update to 5.7

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,5 @@ const { mix } = require('laravel-mix')
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js').version()
-mix.sass('resources/assets/sass/app.scss', 'public/css').version()
+mix.js('resources/js/app.js', 'public/js').version()
+mix.sass('resources/sass/app.scss', 'public/css').version()
