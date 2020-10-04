c149a9a65b65573818cd1554298b0f54b5709ab7
Chenggang Wang <wcgdream@gmail.com>
2020-10-04 15:29:39 +1100

    remove http2 push for laravel 8 compatibility

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -32,7 +32,6 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \JacobBennett\Http2ServerPush\Middleware\AddHttp2ServerPush::class,
         ],
     ];
 
