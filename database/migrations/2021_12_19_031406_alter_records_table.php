f1d9460eba972ddc2414b8d3185a6b3419a6fc7e
Chenggang Wang <wcgdream@gmail.com>
2021-12-19 14:25:31 +1100

    add migration

---
+++ database/migrations/2021_12_19_031406_alter_records_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterRecordsTable extends Migration
+{
+
+    public function up(): void
+    {
+        DB::unprepared('UPDATE `records` SET `original` = `original` * 100, `sale` = `sale` * 100;');
+
+        Schema::table('records', static function (Blueprint $table) {
+            $table->integer('original')->nullable()->change();
+            $table->integer('sale')->nullable()->change();
+            $table->dropColumn('language');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('records', static function (Blueprint $table) {
+            $table->decimal('original_price')->nullable()->change();
+            $table->decimal('sale_price')->nullable()->change();
+            $table->string('language', 20)->default('US')->after('cc');
+        });
+
+        DB::unprepared('UPDATE `records` SET `original` = `original` / 100, `sale` = `sale` / 100;');
+    }
+}
