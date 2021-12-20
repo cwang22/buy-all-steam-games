32b7cc331045ec134583a285c1a62e413a12a30a
Chenggang Wang <cwang22@users.noreply.github.com>
2021-12-20 10:19:13 +0000

    Apply fixes from StyleCI

--- config/queue.php
+++ config/queue.php
@@ -82,7 +82,7 @@ return [
     'failed' => [
         'database' => env('DB_CONNECTION', 'mysql'),
         'table'    => 'failed_jobs',
-        'driver' => 'database-uuids'
+        'driver'   => 'database-uuids',
     ],
 
 ];
