d507e0423884eb2a0d8ef43f7ee0b2219a367c78
Chenggang Wang <wcgdream@gmail.com>
2024-09-09 23:58:17 +1000

    update copy

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,18 +22,19 @@
                 been updated since 2014.</p>
             <p>This page however is based on PHP and Laravel, and automatically fetch daily.</p>
             <h2 class="mt-5">API</h2>
-            <p>You can hit <code>{{url('api')}}</code> to get all the data. It should look like this.</p>
+            <p>You can hit <code>{{url('api/records')}}</code> to get all the data. It should look like this.</p>
             <pre>
-            [
-                {
-                    "original": 233448.27,
-                    "sale": 229259.34,
-                    "cc": "US"
-                    "language": "English"
-                    "created_at": "2017-04-18 14:10:59"
-                },
-                ...
-            ]
+            {
+                "data": [
+                    {
+                        "id": 1,
+                        "original": 233448.27,
+                        "sale": 229259.34,
+                        "created_at": "2017-04-24T20:27:36+00:00"
+                    },
+                    ...
+                ]
+            }
             </pre>
         </div>
     </div>
